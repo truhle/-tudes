ce60b800174bee5b55ce64dacd2ed3ddf0ca85fe
Todd Ruhlen <rhythmtome@yahoo.com>
2016-03-01 13:36:01 -0500

    Through Étude 2-4

--- geom.ex
+++ geom.ex
@@ -1,5 +1,16 @@
 defmodule Geom do
-  def area(height, width) do
+  @moduledoc """
+  Functions calculating gemoetric values.
+  """
+
+  @doc """
+  Takes a height and a width and returns the area of rectangular object.
+  Default values for height and width are both 1.
+  """
+
+  @spec area(number, number) :: number
+  
+  def area(height \\ 1, width \\ 1) do
     height * width
   end
 end
